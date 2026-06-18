c2918f3cff77283273fe67c51321514a4ccbfeb6
da3734120-debug <da3734120@gmail.com>
2026-06-19 01:39:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -471,8 +471,9 @@ class QuickButtonView(discord.ui.View):
         super().__init__(timeout=timeout)
         self.allowed_user = allowed_user
         self.value = None
-     async def handle_click(self, interaction: discord.Interaction, value: str):
-        if interaction.user.id!= self.allowed_user.id:
+
+    async def handle_click(self, interaction: discord.Interaction, value: str):
+        if interaction.user.id != self.allowed_user.id:
             await interaction.response.send_message("❌ You are not allowed to use this button!", ephemeral=True)
             return
         self.value = value
@@ -486,7 +487,6 @@ class QuickButtonView(discord.ui.View):
     @discord.ui.button(label="Decline ❌", style=discord.ButtonStyle.red)
     async def decline_btn(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.handle_click(interaction, "decline")
-
 if __name__ == "__main__":
     keep_alive()
     token = os.getenv('DISCORD_TOKEN')
